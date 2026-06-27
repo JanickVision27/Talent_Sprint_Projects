f49c484ec6a3c88393f206fc10e7be48bcfe7917
Venkata Vikranth Jannatha <vjannatha@gmail.com>
2026-06-27 16:10:42 +0530

    push

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,11 +6,6 @@ public class App {
 
         Date_Calculator date_Calculator = new Date_Calculator();
         date_Calculator.setVisible(true);
-        // LocalDate startDate = LocalDate.of(2004, 6, 27);
-        // LocalDate endDate = LocalDate.of(2026, 6, 27);
-        // date_Calculator.calculateTotalDays(startDate, endDate);
-        // date_Calculator.calculateYearMonthDays(startDate, endDate);
-        // date_Calculator.calculateWeeksAndDays(startDate, endDate);
         
 
 
