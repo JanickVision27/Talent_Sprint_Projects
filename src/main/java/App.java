87410ca5fd4c0310088a5e6d3b9e059853efb632
Venkata Vikranth Jannatha <vjannatha@gmail.com>
2026-06-27 16:41:39 +0530

    push

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
 
         Date_Calculator date_Calculator = new Date_Calculator();
-        date_Calculator.setVisible(true);
+        // date_Calculator.setVisible(true);
         
 
 
