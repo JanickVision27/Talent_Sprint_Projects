c19763de50417ed323341e0c8d49b7ca5d91bf47
Venkata Vikranth Jannatha <vjannatha@gmail.com>
2026-07-13 10:51:45 +0530

    push

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,10 +6,9 @@ public class App {
 
         Date_Calculator date_Calculator = new Date_Calculator();
         // date_Calculator.setVisible(true);
-        
 
-
-
+        System.out.println("Success! Your project is running Java Version: "
+                + System.getProperty("java.version"));
 
     }
 }
